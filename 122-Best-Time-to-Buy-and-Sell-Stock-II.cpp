1054c5248e1a8877da338aa5f6d35404c3687b9a
aniket-1024 <111097120+aniket-1024@users.noreply.github.com>
2024-08-25 15:31:13 +0530

    [122] [Time Beats: 100.00%] [Memory Beats: 35.69%] - LeetPush

--- 122-Best-Time-to-Buy-and-Sell-Stock-II.cpp
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int max = 0;
-        int start = prices[0];
-        int len = prices.size();
-        for(int i = 1;i<len; i++){
-            if(start < prices[i]){
-                max += prices[i] - start;
-            }
-            start = prices[i];
+    int solve(int st, int i, vector<int> &prices) {
+        if(i>=prices.size()) return 0;
+
+        int ans = 0;
+        if(prices[i] < prices[st]) {
+            ans = ans + solve(st+1, i+1, prices);
+        } else {
+            ans = ans + prices[i]-prices[st] + solve(st+1,i+1,prices);
         }
-        return max;
+        return ans;
+    }
+    int maxProfit(vector<int>& prices) {
+        return solve(0,1,prices);
     }
 };
