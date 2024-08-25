be83943b06f38dda6c0a86f14ea8821d0243d7e6
aniket-1024 <111097120+aniket-1024@users.noreply.github.com>
2024-08-25 10:44:00 +0530

    [974] [Time Beats: 24.78%] [Memory Beats: 17.73%] - LeetPush

---
+++ 974-Subarray-Sums-Divisible-by-K.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int sum = 0;
+        unordered_map<int, int> mp;
+        int ans = 0;
+        mp[0] = 1;
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            int rem = sum%k;
+            if(rem<0){
+                rem+=k;
+            }
+            if(mp.find(rem)!=mp.end()){
+                ans+=mp[rem];
+            }
+            mp[rem]++;
+
+        }
+        return ans;
+    }
+};
