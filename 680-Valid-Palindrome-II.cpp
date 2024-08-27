f9432476723b2f4fd99d90ee005cc21b737cffbe
aniket-1024 <111097120+aniket-1024@users.noreply.github.com>
2024-08-27 13:59:39 +0530

    [680] [Time Beats: 57.89%] [Memory Beats: 85.28%] - LeetPush

--- 680-Valid-Palindrome-II.cpp
+++ 680-Valid-Palindrome-II.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    bool solve(string &s, int l, int r,int threshold) {
-        if(threshold < 0) return false;
-        if(l>=r && threshold>=0) return true;
-        int ans = false;
-        if(s[l] == s[r]) ans = ans || solve(s,l+1,r-1,threshold);
-        else {
-            ans = ans || solve(s,l+1,r,threshold-1);
-            if(ans==false) ans = ans || solve(s,l,r-1,threshold-1);
+    bool isPal(string &s, int l, int r) {
+        while(l<=r) {
+            if(s[l] == s[r]) {
+                l++;
+                r--;
+            } else {
+                return false;
+            }
         }
-        return ans;
+        return true;
     }
     bool validPalindrome(string s) {
-        return solve(s,0,s.size()-1,1);
+        int l=0, r=s.size()-1;
+
+        while(l<=r) {
+            if(s[l] == s[r]) {
+                l++;
+                r--;
+            }
+            else {
+                return isPal(s,l+1,r)+isPal(s,l,r-1);
+            }
+        }
+        return true;
     }
 };
