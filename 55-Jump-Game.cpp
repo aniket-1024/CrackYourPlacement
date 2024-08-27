009196582ae1e7b1cb2bbbbd9da3aab8e29d909a
aniket-1024 <111097120+aniket-1024@users.noreply.github.com>
2024-08-27 15:06:38 +0530

    [55] [Time Beats: 44.19%] [Memory Beats: 37.87%] - LeetPush

--- 55-Jump-Game.cpp
+++ 55-Jump-Game.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
-    bool solve(vector<int> &nums, int i, vector<int> &dp) {
-        if(i>=nums.size()-1) return true;
-        if(nums[i] == 0) return false;
-        
-        if(dp[i] != -1) return dp[i];
-        bool ans = false;   
-
-        for(int jump=1; jump<=nums[i]; jump++) {
-            ans = ans || solve(nums, i+jump,dp);
-            if(ans == true) return dp[i] = ans;
-        }
-        return dp[i] = ans;
-    }
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,-1);
-        return solve(nums,0,dp);
+        int n = nums.size(), minJump = 0;
+        for(int i=n-2; i>=0; i--) {
+            minJump++;
+            if(nums[i] >= minJump) minJump = 0;
+        }
+        if(minJump == 0) return true;
+        return false;
     }
 };
